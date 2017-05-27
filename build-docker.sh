9d8f2b3c4b69f15811d0b0d92bb5fc770e9807fe
DrMoriay <dr@DrMori.local>
2017-05-27 16:56:38 +0800

    add a server config

--- build-docker.sh
+++ build-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+cd Docker;
 docker -v  1>/dev/null 2>&1  && printf "[+] check docker\n";
 tar -czf spide.tar.gz spide && printf "[+] tar web project \n";
 docker build -t social-spide . &&  printf "[+] build images ok  'docker images ' to see ....\n";
